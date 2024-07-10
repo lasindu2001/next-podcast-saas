86d3de8ee5de51df537da31cec0266957f293f85
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-11 01:12:45 +0530

    create file and folder structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div>
+            <main>
+                <p className="text-white-1">LeftSidebar</p>
+                {children}
+                <p className="text-white-1">LeftSidebar</p>
+            </main>
+        </div>
+    )
+}
---
+++ app/(root)/profile/[profileId]/page.tsx
@@ -0,0 +1,7 @@
+const Profile = () => {
+  return (
+    <p className='text-white-1'>Profile</p>
+  )
+}
+
+export default Profile
